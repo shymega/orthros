be2b9dc1b9e6451ed7fdf2910136cc17818879f6
Dom Rodriguez <shymega@shymega.org.uk>
2025-07-07 01:18:43 +0100

    refactor: Embed `devenv` config in Flake

--- devshell.nix
+++ devshell.nix
@@ -8,6 +8,34 @@
 inputs.devenv.lib.mkShell {
   inherit inputs pkgs;
   modules = [
-    ./devenv.nix
+    ({pkgs, ...}: {
+      packages = [pkgs.git];
+
+      languages = {
+        rust.enable = true;
+        shell.enable = true;
+        nix.enable = true;
+      };
+
+      services.postgres = {
+        enable = true;
+        package = pkgs.postgresql;
+        listen_addresses = "127.0.0.1";
+        initialDatabases = [{name = "orthros_db";}];
+      };
+
+      devcontainer.enable = true;
+      difftastic.enable = true;
+      dotenv.enable = true;
+
+      git-hooks.hooks = {
+        actionlint.enable = true;
+        commitizen.enable = true;
+        markdownlint.enable = true;
+        deadnix.enable = true;
+        alejandra.enable = true;
+        statix.enable = true;
+      };
+    })
   ];
 }
